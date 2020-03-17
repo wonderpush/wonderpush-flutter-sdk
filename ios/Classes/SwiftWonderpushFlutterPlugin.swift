6b1c6e8e07dbe204632f62913757479c8458198f
mandeep.kaur <mandeep.kaur@infostride.com>
2020-03-17 17:57:06 +0530

    ios methods added

--- ios/Classes/SwiftWonderpushFlutterPlugin.swift
+++ ios/Classes/SwiftWonderpushFlutterPlugin.swift
@@ -1,5 +1,6 @@
 import Flutter
 import UIKit
+import WonderPush
 
 public class SwiftWonderpushFlutterPlugin: NSObject, FlutterPlugin {
   public static func register(with registrar: FlutterPluginRegistrar) {
@@ -9,6 +10,31 @@ public class SwiftWonderpushFlutterPlugin: NSObject, FlutterPlugin {
   }
 
   public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
-    result("iOS " + UIDevice.current.systemVersion)
+    
+    switch call.method  {
+        
+    case "init":
+       
+        if let arguments = call.arguments as? [String:Any],let clientId = arguments["clientId"] as? String, let clientSecret = arguments["clientSecret"] as? String{
+            WonderPush.setClientId(clientId, secret: clientSecret)
+           
+             WonderPush.setupDelegateForUserNotificationCenter()
+            result("Wonderpush initalized successfully");
+        }else{
+            result("Wonderpush initalized failed due to data mismatch datatype");
+        }
+    case "trackEvent":
+        if let arguments = call.arguments as? [String:Any],let eventType = arguments["eventType"] as? String{
+            WonderPush.trackEvent(eventType)
+            result("Subscribed to event "+eventType+"successfully")
+        }
+
+    default:
+        result("iOS " + UIDevice.current.systemVersion)
+        
+    }
   }
+    
+    
+    
 }
