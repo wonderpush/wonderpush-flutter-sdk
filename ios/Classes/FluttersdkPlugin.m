45449770863183ca3ef2c1b21bbf0db3b99ac15e
Rakesh <ninjacoderrakesh@gmailcom>
2020-04-27 15:55:11 +0530

    Flutter SDK initial commit for structure setup

---
+++ ios/Classes/FluttersdkPlugin.m
@@ -0,0 +1,20 @@
+#import "FluttersdkPlugin.h"
+
+@implementation FluttersdkPlugin
++ (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
+  FlutterMethodChannel* channel = [FlutterMethodChannel
+      methodChannelWithName:@"fluttersdk"
+            binaryMessenger:[registrar messenger]];
+  FluttersdkPlugin* instance = [[FluttersdkPlugin alloc] init];
+  [registrar addMethodCallDelegate:instance channel:channel];
+}
+
+- (void)handleMethodCall:(FlutterMethodCall*)call result:(FlutterResult)result {
+  if ([@"getPlatformVersion" isEqualToString:call.method]) {
+    result([@"iOS " stringByAppendingString:[[UIDevice currentDevice] systemVersion]]);
+  } else {
+    result(FlutterMethodNotImplemented);
+  }
+}
+
+@end
