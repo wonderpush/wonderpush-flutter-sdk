97fb1d9200cc96ac9018a48b869f83c58bf101dc
Olivier Favre <olivier@wonderpush.com>
2021-05-03 17:18:14 +0200

    Android: io.flutter.plugin.common.PluginRegistry.Registrar is deprecated

--- android/src/main/java/com/wonderpush/sdk/flutter/WonderPushPlugin.java
+++ android/src/main/java/com/wonderpush/sdk/flutter/WonderPushPlugin.java
@@ -19,7 +19,6 @@ import io.flutter.plugin.common.MethodCall;
 import io.flutter.plugin.common.MethodChannel;
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
 import io.flutter.plugin.common.MethodChannel.Result;
-import io.flutter.plugin.common.PluginRegistry.Registrar;
 import android.location.Location;
 
 import com.wonderpush.sdk.DeepLinkEvent;
@@ -128,19 +127,6 @@ public class WonderPushPlugin implements FlutterPlugin, MethodCallHandler {
                 }
             }
         }, 100);
-    }
-
-    // This static function is optional and equivalent to onAttachedToEngine. It supports the old
-    // pre-Flutter-1.12 Android projects. You are encouraged to continue supporting
-    // plugin registration via this function while apps migrate to use the new Android APIs
-    // post-flutter-1.12 via https://flutter.dev/go/android-project-migration.
-    //
-    // It is encouraged to share logic between onAttachedToEngine and registerWith to keep
-    // them functionally equivalent. Only one of onAttachedToEngine or registerWith will be called
-    // depending on the user's project. onAttachedToEngine or registerWith must both be defined
-    // in the same class.
-    public static void registerWith(Registrar registrar) {
-        WonderPushPlugin.getInstance().onAttachedToEngine(registrar.context().getApplicationContext(), registrar.messenger());
     }
 
     public static void setupWonderPushDelegate() {
